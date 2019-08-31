d8d5b38b8ea453b0f84549f99e994765df9fb004
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-08-31 17:32:54 +0530

    contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,22 +12,27 @@ const IndexPage = () => (
     <form name="contact Form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact Form" />
+
       <div className="field half first">
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" />
       </div>
+
       <div className="field half first">
         <label htmlFor="name">Last Name</label>
         <input type="text" name="lastname" id="lastname" />
       </div>
+
       <div className="field half">
         <label htmlFor="email">Email</label>
         <input type="text" name="email" id="email" />
       </div>
+
       <div className="field">
         <label htmlFor="message">Message</label>
         <textarea name="message" id="message" rows="6" />
       </div>
+
       <ul className="actions">
         <li>
           <input type="submit" value="Send Message" className="special" />
@@ -36,6 +41,7 @@ const IndexPage = () => (
           <input type="reset" value="Clear" />
         </li>
       </ul>
+      
     </form>
 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
